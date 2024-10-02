f0bf366267487d2fa150c21819fb6ca624b6f0b2
Bruno <bruno.uemura1@gmail.com>
2024-10-02 13:47:59 -0300

    feat: add player move logic

--- src/FlappyBird.java
+++ src/FlappyBird.java
@@ -1,7 +1,9 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class FlappyBird extends JPanel {
+public class FlappyBird extends JPanel implements ActionListener {
 
     Image backgroudImage;
     Image birdImage;
@@ -28,10 +30,12 @@ public class FlappyBird extends JPanel {
 
     // Game Logic
     Bird bird;
+    int velocityY = -6;
+
+    Timer gameLoop;
 
     FlappyBird() {
         setPreferredSize(new Dimension(Board.width, Board.height));
-//        setBackground(Color.blue);
 
         // Load imagesß
         backgroudImage = new ImageIcon(getClass().getResource("./assets/flappybirdbg.png")).getImage();
@@ -39,7 +43,12 @@ public class FlappyBird extends JPanel {
         topPipeImage = new ImageIcon(getClass().getResource("./assets/toppipe.png")).getImage();
         bottomPipeImage = new ImageIcon(getClass().getResource("./assets/bottompipe.png")).getImage();
 
+        // bird
         bird = new Bird(birdImage);
+
+        // game timer
+        gameLoop = new Timer(1000/60, this);
+        gameLoop.start();
     }
 
     public void paintComponent(Graphics graphics) {
@@ -48,6 +57,20 @@ public class FlappyBird extends JPanel {
     }
 
     public void draw(Graphics graphics) {
-        graphics.drawImage(backgroudImage, 0, 0, Board.width, Board.height, null);
+        System.out.println("draw");
+
+        graphics.drawImage(backgroudImage, 0, 0, Board.width, Board.height, null); // Draw background
+        graphics.drawImage(birdImage, bird.x, bird.y, bird.width, bird.height, null); // Draw player
+    }
+
+    public void move() {
+        // bird
+        bird.y += velocityY;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        move();
+        repaint();
     }
 }
