2e9cac76def50ae0ed7bb292b089ce0d480e4131
Bruno <bruno.uemura1@gmail.com>
2024-10-02 13:38:12 -0300

    refactor: move board to its own class

--- src/Board.java
+++ src/Board.java
@@ -1,4 +1,20 @@
-public class Board {
+import javax.swing.*;
+
+public class Board extends JFrame {
     static int width = 360;
     static int height = 640;
+
+    Board(String boardName) {
+        super(boardName);
+        super.setSize(Board.width, Board.height);
+        super.setLocationRelativeTo(null);
+        super.setResizable(false);
+        super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public void start(JPanel panel) {
+        super.add(panel);
+        super.pack();
+        super.setVisible(true);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,7 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-
-        JFrame frame = new JFrame("Flappy Bird");
-        frame.setSize(Board.width, Board.height);
-        frame.setLocationRelativeTo(null);
-        frame.setResizable(false);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        FlappyBird flappyBird = new FlappyBird();
-        frame.add(flappyBird);
-        frame.pack();
-        frame.setVisible(true);
+        Board board = new Board("Flappy Bird");
+        board.start(new FlappyBird());
     }
 }
